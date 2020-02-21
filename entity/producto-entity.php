5ca400cce1720f16f9318b7caf32bd1515fd39de
Jose1997ureta <jose1997ureta@gmail.com>
2020-02-20 22:52:46 -0500

    se termino las clases de la entidad

---
+++ entity/producto-entity.php
@@ -0,0 +1,365 @@
+<?php
+class productoEntity {
+
+    public $idProducto;
+    public $imagenResultadoProducto;
+    public $rutaVideoProducto;
+    public $flagProducto;
+
+    public $tipoProductoES;
+    public $nombreProductoES;
+    public $descripcionProductoES;
+    public $descripcionItemProductoES;
+    public $tituloBeneficioES;
+    public $descripcionBeneficioES;
+    public $tituloPrincipiosActivosES;
+    public $descripcionPrincipiosActivosES;
+
+    public $tipoProductoEN;
+    public $nombreProductoEN;
+    public $descripcionProductoEN;
+    public $descripcionItemProductoEN;
+    public $tituloBeneficioEN;
+    public $descripcionBeneficioEN;
+    public $tituloPrincipiosActivosEN;
+    public $descripcionPrincipiosActivosEN;
+
+    public $tipoProductoRU;
+    public $nombreProductoRU;
+    public $descripcionProductoRU;
+    public $descripcionItemProductoRU;
+    public $tituloBeneficioRU;
+    public $descripcionBeneficioRU;
+    public $tituloPrincipiosActivosRU;
+    public $descripcionPrincipiosActivosRU;
+
+    public $tipoProductoFR;
+    public $nombreProductoFR;
+    public $descripcionProductoFR;
+    public $descripcionItemProductoFR;
+    public $tituloBeneficioFR;
+    public $descripcionBeneficioFR;
+    public $tituloPrincipiosActivosFR;
+    public $descripcionPrincipiosActivosFR;
+
+    public $idImagen;
+    public $imagenProducto;
+
+    // GETTER
+
+    public function getIdProducto(){
+        return $this->idProducto;
+    }
+
+    public function getImagenResultadoProducto(){
+        return $this->imagenResultadoProducto;
+    }
+
+    public function getRutaVideoProducto(){
+        return $this->rutaVideoProducto;
+    }
+
+    public function getFlagProducto(){
+        return $this->flagProducto;
+    }
+
+    // ===============================================
+    public function getTipoProductoES(){
+        return $this->tipoProductoES;
+    }
+
+    public function getnombreProductoES(){
+        return $this->nombreProductoES;
+    }
+
+    public function getDescripcionProductoES(){
+        return $this->descripcionProductoES;
+    }
+
+    public function getDescripcionItemProductoES(){
+        return $this->descripcionItemProductoES;
+    }
+
+    public function getTituloBeneficioES(){
+        return $this->tituloBeneficioES;
+    }
+
+    public function getDescripcionBeneficioES(){
+        return $this->descripcionBeneficioES;
+    }
+
+    public function getTituloPrincipiosActivosES(){
+        return $this->tituloPrincipiosActivosES;
+    }
+
+    public function getDescripcionPrincipiosActivosES(){
+        return $this->descripcionPrincipiosActivosES;
+    }
+    
+    // ===============================================
+    public function getTipoProductoEN(){
+        return $this->tipoProductoEN;
+    }
+
+    public function getnombreProductoEN(){
+        return $this->nombreProductoEN;
+    }
+
+    public function getDescripcionProductoEN(){
+        return $this->descripcionProductoEN;
+    }
+
+    public function getDescripcionItemProductoEN(){
+        return $this->descripcionItemProductoEN;
+    }
+
+    public function getTituloBeneficioEN(){
+        return $this->tituloBeneficioEN;
+    }
+
+    public function getDescripcionBeneficioEN(){
+        return $this->descripcionBeneficioEN;
+    }
+
+    public function getTituloPrincipiosActivosEN(){
+        return $this->tituloPrincipiosActivosEN;
+    }
+
+    public function getDescripcionPrincipiosActivosEN(){
+        return $this->descripcionPrincipiosActivosEN;
+    }
+
+
+    // ===============================================
+    public function getTipoProductoRU(){
+        return $this->tipoProductoRU;
+    }
+
+    public function getnombreProductoRU(){
+        return $this->nombreProductoRU;
+    }
+
+    public function getDescripcionProductoRU(){
+        return $this->descripcionProductoRU;
+    }
+
+    public function getDescripcionItemProductoRU(){
+        return $this->descripcionItemProductoRU;
+    }
+
+    public function getTituloBeneficioRU(){
+        return $this->tituloBeneficioRU;
+    }
+
+    public function getDescripcionBeneficioRU(){
+        return $this->descripcionBeneficioRU;
+    }
+
+    public function getTituloPrincipiosActivosRU(){
+        return $this->tituloPrincipiosActivosRU;
+    }
+
+    public function getDescripcionPrincipiosActivosRU(){
+        return $this->descripcionPrincipiosActivosRU;
+    }
+    
+
+    // ===============================================
+    public function getTipoProductoFR(){
+        return $this->tipoProductoFR;
+    }
+
+    public function getnombreProductoFR(){
+        return $this->nombreProductoFR;
+    }
+
+    public function getDescripcionProductoFR(){
+        return $this->descripcionProductoFR;
+    }
+
+    public function getDescripcionItemProductoFR(){
+        return $this->descripcionItemProductoFR;
+    }
+
+    public function getTituloBeneficioFR(){
+        return $this->tituloBeneficioFR;
+    }
+
+    public function getDescripcionBeneficioFR(){
+        return $this->descripcionBeneficioFR;
+    }
+
+    public function getTituloPrincipiosActivosFR(){
+        return $this->tituloPrincipiosActivosFR;
+    }
+
+    public function getDescripcionPrincipiosActivosFR(){
+        return $this->descripcionPrincipiosActivosFR;
+    }
+
+    // ================
+    public function getIdImagen(){
+        return $this->idImagen;
+    }
+
+    public function getImagenProducto(){
+        return $this->imagenProducto;
+    }
+    
+    // SETTER
+    public function setIdProducto($idProducto){
+        return $this->idProducto = $idProducto;
+    }
+
+    public function setImagenResultadoProducto($imagenResultadoProducto){
+        return $this->imagenResultadoProducto = $imagenResultadoProducto;
+    }
+
+    public function setRutaVideoProducto($rutaVideoProducto){
+        return $this->rutaVideoProducto = $rutaVideoProducto;
+    }
+
+    public function setFlagProducto($flagProducto){
+        return $this->flagProducto = $flagProducto;
+    }
+
+    // ==================
+    public function setTipoProductoES($tipoProductoES){
+        return $this->tipoProductoES = $tipoProductoES;
+    }
+
+    public function setnombreProductoES($nombreProductoES){
+        return $this->nombreProductoES = $nombreProductoES;
+    }
+
+    public function setDescripcionProductoES($descripcionProductoES){
+        return $this->descripcionProductoES = $descripcionProductoES;
+    }
+
+    public function setDescripcionItemProductoES($descripcionItemProductoES){
+        return $this->descripcionItemProductoES = $descripcionItemProductoES;
+    }
+
+    public function setTituloBeneficioES($tituloBeneficioES){
+        return $this->tituloBeneficioES = $tituloBeneficioES;
+    }
+
+    public function setDescripcionBeneficioES($descripcionBeneficioES){
+        return $this->descripcionBeneficioES = $descripcionBeneficioES;
+    }
+
+    public function setTituloPrincipiosActivosES($tituloPrincipiosActivosES){
+        return $this->tituloPrincipiosActivosES = $tituloPrincipiosActivosES;
+    }
+
+    public function setDescripcionPrincipiosActivosES($descripcionPrincipiosActivosES){
+        return $this->descripcionPrincipiosActivosES = $descripcionPrincipiosActivosES;
+    }
+
+
+    public function setTipoProductoEN($tipoProductoEN){
+        return $this->tipoProductoEN = $tipoProductoEN;
+    }
+
+    public function setnombreProductoEN($nombreProductoEN){
+        return $this->nombreProductoEN = $nombreProductoEN;
+    }
+
+    public function setDescripcionProductoEN($descripcionProductoEN){
+        return $this->descripcionProductoEN = $descripcionProductoEN;
+    }
+
+    public function setDescripcionItemProductoEN($descripcionItemProductoEN){
+        return $this->descripcionItemProductoEN = $descripcionItemProductoEN;
+    }
+
+    public function setTituloBeneficioEN($tituloBeneficioEN){
+        return $this->tituloBeneficioEN = $tituloBeneficioEN;
+    }
+
+    public function setDescripcionBeneficioEN($descripcionBeneficioEN){
+        return $this->descripcionBeneficioEN = $descripcionBeneficioEN;
+    }
+
+    public function setTituloPrincipiosActivosEN($tituloPrincipiosActivosEN){
+        return $this->tituloPrincipiosActivosEN = $tituloPrincipiosActivosEN;
+    }
+
+    public function setDescripcionPrincipiosActivosEN($descripcionPrincipiosActivosEN){
+        return $this->descripcionPrincipiosActivosEN = $descripcionPrincipiosActivosEN;
+    }
+
+
+    public function setTipoProductoRU($tipoProductoRU){
+        return $this->tipoProductoRU = $tipoProductoRU;
+    }
+
+    public function setnombreProductoRU($nombreProductoRU){
+        return $this->nombreProductoRU = $nombreProductoRU;
+    }
+
+    public function setDescripcionProductoRU($descripcionProductoRU){
+        return $this->descripcionProductoRU = $descripcionProductoRU;
+    }
+
+    public function setDescripcionItemProductoRU($descripcionItemProductoRU){
+        return $this->descripcionItemProductoRU = $descripcionItemProductoRU;
+    }
+
+    public function setTituloBeneficioRU($tituloBeneficioRU){
+        return $this->tituloBeneficioRU = $tituloBeneficioRU;
+    }
+
+    public function setDescripcionBeneficioRU($descripcionBeneficioRU){
+        return $this->descripcionBeneficioRU = $descripcionBeneficioRU;
+    }
+
+    public function setTituloPrincipiosActivosRU($tituloPrincipiosActivosRU){
+        return $this->tituloPrincipiosActivosRU = $tituloPrincipiosActivosRU;
+    }
+
+    public function setDescripcionPrincipiosActivosRU($descripcionPrincipiosActivosRU){
+        return $this->descripcionPrincipiosActivosRU = $descripcionPrincipiosActivosRU;
+    }
+
+
+    public function setTipoProductoFR($tipoProductoFR){
+        return $this->tipoProductoFR = $tipoProductoFR;
+    }
+
+    public function setnombreProductoFR($nombreProductoFR){
+        return $this->nombreProductoFR = $nombreProductoFR;
+    }
+
+    public function setDescripcionProductoFR($descripcionProductoFR){
+        return $this->descripcionProductoFR = $descripcionProductoFR;
+    }
+
+    public function setDescripcionItemProductoFR($descripcionItemProductoFR){
+        return $this->descripcionItemProductoFR = $descripcionItemProductoFR;
+    }
+
+    public function setTituloBeneficioFR($tituloBeneficioFR){
+        return $this->tituloBeneficioFR = $tituloBeneficioFR;
+    }
+
+    public function setDescripcionBeneficioFR($descripcionBeneficioFR){
+        return $this->descripcionBeneficioFR = $descripcionBeneficioFR;
+    }
+
+    public function setTituloPrincipiosActivosFR($tituloPrincipiosActivosFR){
+        return $this->tituloPrincipiosActivosFR = $tituloPrincipiosActivosFR;
+    }
+
+    public function setDescripcionPrincipiosActivosFR($descripcionPrincipiosActivosFR){
+        return $this->descripcionPrincipiosActivosFR = $descripcionPrincipiosActivosFR;
+    }
+
+    public function setIdImagen($idImagen){
+        return $this->idImagen = $idImagen;
+    }
+
+    public function setImagenProducto($imagenProducto){
+        return $this->imagenProducto = $imagenProducto;
+    }
+}
